2bad1ffca37a49aab9e1eae3773e18ddde199d8f
Apeirogon99 <apeirogon99@gmail.com>
2026-01-15 14:01:43 +0900

    ci: [BE] mysql 연결 설정 및 example 삭제

--- storage/db-core/src/main/java/com/team/voteland/storage/db/core/ExampleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.team.voteland.storage.db.core;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExampleRepository extends JpaRepository<ExampleEntity, Long> {
-
-}
